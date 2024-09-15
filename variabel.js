ebdbb27e2b8f35e6cb21a8dc6e6b0e57e6282337
Rakhshanda Shaina Ahava Hidayat <shainaahava25@gmail.com>
2024-09-15 02:18:02 -0700

    Create variabel.js

---
+++ variabel.js
@@ -0,0 +1,14 @@
+let usia = 30;
+let tinggi = 175;
+let berat = 70;
+let suhuAir = 25;
+const pi = 3.14;
+const tahun = 2024;
+
+document.getElementById('usia').textContent += usia;
+document.getElementById('tinggi').textContent += tinggi;
+document.getElementById('berat').textContent += berat;
+document.getElementById('suhu').textContent += suhuAir;
+document.getElementById('pi').textContent += pi;
+document.getElementById('tahun').textContent += tahun;
+
